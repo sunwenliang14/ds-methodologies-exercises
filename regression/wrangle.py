937e6af7dbcc2588a5c7b0c86d1be6a7ca65cfdc
Wenliang Sun <wenliangsonny@gmail.com>
2020-04-07 15:56:52 -0500

    m

--- regression/wrangle.py
+++ regression/wrangle.py
@@ -18,3 +18,9 @@ def wrangle_telco():
     df = df.dropna() 
     return df
    
+def wrangle_grades():
+    grades = pd.read_csv("student_grades.csv")
+    grades.drop(columns="student_id", inplace=True)
+    grades.replace(r"^\s*$", np.nan, regex=True, inplace=True)
+    df = grades.dropna().astype("int")
+    return df
