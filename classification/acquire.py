5258357050dc866d7ca74aea5eaefe24ecbccf2f
Wenliang Sun <wenliangsonny@gmail.com>
2020-04-14 00:10:47 -0500

    m

---
+++ classification/acquire.py
@@ -0,0 +1,16 @@
+import env
+import pandas as pd
+import seaborn as sns
+
+def get_connection(db, user=env.user, host=env.host, password=env.password):
+    return f'mysql+pymysql://{user}:{password}@{host}/{db}'
+
+
+
+def get_titanic_data():
+    return pd.read_sql('SELECT * FROM passengers', get_connection('titanic_db'))
+
+
+
+def get_iris_data():
+    return pd.read_sql('select * from measurements join species using (species_id)', get_connection('iris_db'))
